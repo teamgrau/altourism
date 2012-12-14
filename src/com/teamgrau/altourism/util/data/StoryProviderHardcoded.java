c8fa0107b6f450b38bf8b687cf276e8466961cdd
Simon <simon.wichmann@fu-berlin.de>
2012-12-14 09:56:57 +0100

    Startet implementing issue #22

---
+++ src/com/teamgrau/altourism/util/data/StoryProviderHardcoded.java
@@ -0,0 +1,16 @@
+package com.teamgrau.altourism.util.data;
+
+import java.util.List;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.teamgrau.altourism.util.data.model.POI;
+
+public class StoryProviderHardcoded implements StoryProvider {
+
+	@Override
+	public List<POI> listPOIs(LatLng position, double radius) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
