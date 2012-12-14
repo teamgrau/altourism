fcd7ca9575123bf82cbcd0d1b4152663b665e21e
Simon <simon.wichmann@fu-berlin.de>
2012-12-14 09:44:41 +0100

    Implemented issue #11

---
+++ src/com/teamgrau/altourism/util/data/POI.java
@@ -0,0 +1,29 @@
+package com.teamgrau.altourism.util.data;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * POI models the data-object of an interesting point.
+ * 
+ * @author simon
+ *
+ */
+
+public final class POI {
+	
+	String title;
+	LatLng position;
+	List<Geschichte> geschichten;
+	
+	
+	public POI( String title, LatLng position ) {
+		this.title = title;
+		this.position = position;
+		geschichten = new ArrayList<Geschichte>();
+		
+	}
+
+}
