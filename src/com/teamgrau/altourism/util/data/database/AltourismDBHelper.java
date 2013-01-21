9d514df88cb781b2cd08238f05ca2dc3b2d39341
Simon <simon.wichmann@fu-berlin.de>
2013-01-21 20:06:00 +0100

    Begonnen, #37 zu implementieren. Bisschen Refactoring, ein paar DB Definitionen erstellt

--- src/com/teamgrau/altourism/util/data/database/AltourismDBHelper.java
+++ src/com/teamgrau/altourism/util/data/database/AltourismDBHelper.java
@@ -16,32 +16,19 @@ public class AltourismDBHelper extends SQLiteOpenHelper{
 
     public static final int DATABASE_VERSION = 1; // Should be incremented on schema update
 
-    private static final String CREATE_TABLE_POSITIONS_STATEMENT =
-            "CREATE TABLE " + DBDefinition.PositionEntry.TABLE_NAME + " (" +
-            DBDefinition.PositionEntry._ID + " INTEGER PRIMARY KEY," +         // Autoincrementing rowid
-            DBDefinition.PositionEntry.COLUMN_NAME_Lat + " REAL," +
-            DBDefinition.PositionEntry.COLUMN_NAME_Lng + " REAL" +
-            ")";
-
-    private static final String DELETE_ENTRIES_TABLE_POSITIONS =
-            "DROP TABLE IF EXITS " + DBDefinition.PositionEntry.TABLE_NAME;
-
-
-
-
     public AltourismDBHelper(Context context) {
         super(context, DBDefinition.DB_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE_POSITIONS_STATEMENT);
+        db.execSQL(DBDefinition.CREATE_TABLE_POSITIONS_STATEMENT);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // At this time we just drop all data and create a new DB out of the definition
-        db.execSQL(DELETE_ENTRIES_TABLE_POSITIONS);
+        db.execSQL(DBDefinition.DELETE_ENTRIES_TABLE_POSITIONS);
         onCreate(db);
     }
 }
--- src/com/teamgrau/altourism/util/data/database/DBDefinition.java
+++ src/com/teamgrau/altourism/util/data/database/DBDefinition.java
@@ -8,13 +8,14 @@ import android.provider.BaseColumns;
  * User: Simon
  */
 
-public final class DBDefinition{
+public final class DBDefinition {
 
     public static final String DB_NAME = "AltourismDB";
 
 
-    // The table that stores GPS-positions
-    public static abstract class PositionEntry implements BaseColumns{
+// The table that stores GPS-positions the user has passed
+    // table definition
+    public static abstract class PositionEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "Positions";
         public static final String COLUMN_NAME_Lat = "Lat";
@@ -22,8 +23,33 @@ public final class DBDefinition{
 
     }
 
+    // Create statement
+    static final String CREATE_TABLE_POSITIONS_STATEMENT =
+            "CREATE TABLE " + PositionEntry.TABLE_NAME + " (" +
+                    PositionEntry._ID + " INTEGER PRIMARY KEY," +    // Autoincrementing rowid
+                    PositionEntry.COLUMN_NAME_Lat + " REAL," +
+                    PositionEntry.COLUMN_NAME_Lng + " REAL" +
+                    ")";
+
+    // Drop statement
+    static final String DELETE_ENTRIES_TABLE_POSITIONS =
+            "DROP TABLE IF EXITS " + PositionEntry.TABLE_NAME;
+
+
+
+// The table that stores geschichten for certain locations
+    // table definition
+    public static abstract class Geschichte implements BaseColumns {
+
+        public static final String TABLE_NAME = "Geschichten";
+        public static final String COLUMN_NAME_LOCATION = "Location";
+        public static final String COLUMN_NAME_GESCHICHTE = "Geschichte";
+    }
+
+    // Create statement
+
 
     // No one should never ever instantiate this class as all information here is static
-    private DBDefinition(){}
+    private DBDefinition() {}
 
 }
